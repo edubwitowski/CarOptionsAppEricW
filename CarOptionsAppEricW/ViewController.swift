e54999f14eeffc2902c7e3584016232e3d04b6f5
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2018-02-11 12:53:15 -0600

    version 1.0

--- CarOptionsAppEricW/ViewController.swift
+++ CarOptionsAppEricW/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var toyImages: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func button1(_ sender: UIButton) {
+        toyImages.image = UIImage(named: "toy_2.jpg")
+    }
+    @IBAction func button2(_ sender: UIButton) {
+        toyImages.image = UIImage(named: "toy_3.jpg")
+    }
+    @IBAction func button3(_ sender: UIButton) {
+        toyImages.image = UIImage(named: "toy_4.jpg")
+    }
+    @IBAction func button4(_ sender: UIButton) {
+        toyImages.image = UIImage(named: "toy_7.png")
+    }
+    @IBAction func button5(_ sender: UIButton) {
+        toyImages.image = UIImage(named: "toy_8.png")
+    }
+    @IBAction func button6(_ sender: UIButton) {
+        toyImages.image = UIImage(named: "toy_.jpg")    }
 }
+
 
